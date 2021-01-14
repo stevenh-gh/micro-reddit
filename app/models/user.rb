ed206be4d37db66ca00a72e525b52110d82bbc79
steven <steven.h@live.com>
2021-01-13 17:06:48 -0800

    add Post model add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 6, maximum: 25 }
   validates :password, presence: true
+
+  has_many :posts
+  has_many :comments
 end
